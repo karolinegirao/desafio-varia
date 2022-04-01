a75ef9edde420d6a3567330794d255df9323e2ac
karolinegirao <86789094+karolinegirao@users.noreply.github.com>
2022-03-31 21:22:31 -0300

    Add files via upload
    atividade importação

---
+++ atividade corrigida.py
@@ -0,0 +1,50 @@
+#Desafio variaveis
+#IMPORTAÇÃO DE BIBLIOTECA
+
+
+
+
+
+
+
+
+
+
+
+#primeira função 
+def valInt ():
+    numero = 500
+    print('O numero  é ', numero, ' !' )
+
+
+def valstr ():
+    nome = 'Karoline de  '
+    print(nome)
+
+
+def valBol ():
+    aprovado = True 
+    print('Você está salvo ? ', aprovado)
+
+def valFloat ():
+    salario = 10000
+    print('O seu salario é : ', salario)
+    
+
+
+
+#programa principal
+
+valInt ()
+valstr ()
+valBol ()
+valFloat ()
+
+
+
+
+
+
+
+
+   
